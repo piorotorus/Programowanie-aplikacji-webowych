2f5699c7f1474ab039c3dcf3d06855686e22ddbd
piorotorus <48321049+piorotorus@users.noreply.github.com>
2020-04-02 19:01:15 +0200

    Add files via upload
    add changes

--- lekcja 3 form creator/FormCreator.js
+++ lekcja 3 form creator/FormCreator.js
@@ -21,29 +21,99 @@ var InputField = /** @class */ (function () {
     }
     InputField.prototype.render = function () {
         var node = document.createElement("LI");
-        var textnode = document.createTextNode(this.value);
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
         node.appendChild(textnode);
+        node.appendChild(inputnode);
         document.getElementById("namelist").appendChild(node);
     };
     return InputField;
 }());
-/*
-class TextAreaField implements Field{
-
-}
-class DateField implements Field{
-
-}
-class EmailField implements Field{
-
-}
-class SelectField implements Field{
-
-}
-class CheckboxField implements Field{
-
-}
-*/
+var TextAreaField = /** @class */ (function () {
+    function TextAreaField(value) {
+        this.name = "TextAreaField";
+        this.label = "etykieta";
+        this.fieldType = FieldType.TextAreaField;
+        this.value = value;
+    }
+    TextAreaField.prototype.render = function () {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+    };
+    return TextAreaField;
+}());
+var DateField = /** @class */ (function () {
+    function DateField(value) {
+        this.name = "DateField";
+        this.label = "etykieta";
+        this.fieldType = FieldType.DateField;
+        this.value = value;
+    }
+    DateField.prototype.render = function () {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+    };
+    return DateField;
+}());
+var EmailField = /** @class */ (function () {
+    function EmailField(value) {
+        this.name = "EmailField";
+        this.label = "etykieta";
+        this.fieldType = FieldType.EmailField;
+        this.value = value;
+    }
+    EmailField.prototype.render = function () {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+    };
+    return EmailField;
+}());
+var SelectField = /** @class */ (function () {
+    function SelectField(value) {
+        this.name = "SelectField";
+        this.label = "etykieta";
+        this.fieldType = FieldType.SelectField;
+        this.value = value;
+    }
+    SelectField.prototype.render = function () {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+    };
+    return SelectField;
+}());
+var CheckboxField = /** @class */ (function () {
+    function CheckboxField(value) {
+        this.name = "CheckboxField";
+        this.label = "etykieta";
+        this.fieldType = FieldType.CheckboxField;
+        this.value = value;
+    }
+    CheckboxField.prototype.render = function () {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(this.name);
+        var inputnode = document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+    };
+    return CheckboxField;
+}());
 var Form = /** @class */ (function () {
     function Form() {
         this.fieldArray = [];
@@ -51,12 +121,24 @@ var Form = /** @class */ (function () {
     Form.prototype.getValue = function () {
     };
     Form.prototype.render = function () {
-        alert("AAA");
-        /*
-        let inputField: InputField = new InputField("A");
+        var inputField = new InputField("A");
         inputField.render();
         this.fieldArray.push(inputField);
-        */
+        var textAreaField = new TextAreaField("A");
+        textAreaField.render();
+        this.fieldArray.push(textAreaField);
+        var dateField = new DateField("A");
+        dateField.render();
+        this.fieldArray.push(dateField);
+        var emailField = new EmailField("A");
+        emailField.render();
+        this.fieldArray.push(emailField);
+        var selectField = new SelectField("A");
+        selectField.render();
+        this.fieldArray.push(selectField);
+        var checkboxField = new CheckboxField("A");
+        inputField.render();
+        this.fieldArray.push(checkboxField);
     };
     return Form;
 }());
@@ -65,4 +147,4 @@ var App = /** @class */ (function () {
     }
     return App;
 }());
-var form = new Form();
+var format = new Form();
--- lekcja 3 form creator/FormCreator.ts
+++ lekcja 3 form creator/FormCreator.ts
@@ -36,44 +36,153 @@ class InputField implements Field {
 
     render() {
 var node=document.createElement("LI");
-    var textnode=document.createTextNode(this.value);
+    var textnode=document.createTextNode(this.name);
+    var inputnode=document.createElement("input");
     node.appendChild(textnode);
+    node.appendChild(inputnode);
     document.getElementById("namelist").appendChild(node);
     }
 }
-/*
+
 class TextAreaField implements Field{
+name:string;
+label: string;
+fieldType: FieldType;
+value: any;
+
+constructor(value: any) {
+    this.name = "TextAreaField";
+    this.label = "etykieta"
+    this.fieldType = FieldType.TextAreaField;
+    this.value = value
+}
+render() {
+    var node=document.createElement("LI");
+        var textnode=document.createTextNode(this.name);
+        var inputnode=document.createElement("input");
+        node.appendChild(textnode);
+        node.appendChild(inputnode);
+        document.getElementById("namelist").appendChild(node);
+        }
 
 }
-class DateField implements Field{
 
+class DateField implements Field{
+    name:string;
+    label: string;
+    fieldType: FieldType;
+    value: any;
+    
+    constructor(value: any) {
+        this.name = "DateField";
+        this.label = "etykieta"
+        this.fieldType = FieldType.DateField;
+        this.value = value
+    }
+    render() {
+        var node=document.createElement("LI");
+            var textnode=document.createTextNode(this.name);
+            var inputnode=document.createElement("input");
+            node.appendChild(textnode);
+            node.appendChild(inputnode);
+            document.getElementById("namelist").appendChild(node);
+            }
 }
 class EmailField implements Field{
-
+    name:string;
+    label: string;
+    fieldType: FieldType;
+    value: any;
+    
+    constructor(value: any) {
+        this.name = "EmailField";
+        this.label = "etykieta"
+        this.fieldType = FieldType.EmailField;
+        this.value = value
+    }
+    render() {
+        var node=document.createElement("LI");
+            var textnode=document.createTextNode(this.name);
+            var inputnode=document.createElement("input");
+            node.appendChild(textnode);
+            node.appendChild(inputnode);
+            document.getElementById("namelist").appendChild(node);
+            }
 }
 class SelectField implements Field{
-
+    name:string;
+    label: string;
+    fieldType: FieldType;
+    value: any;
+    
+    constructor(value: any) {
+        this.name = "SelectField";
+        this.label = "etykieta"
+        this.fieldType = FieldType.SelectField;
+        this.value = value
+    }
+    render() {
+        var node=document.createElement("LI");
+            var textnode=document.createTextNode(this.name);
+            var inputnode=document.createElement("input");
+            node.appendChild(textnode);
+            node.appendChild(inputnode);
+            document.getElementById("namelist").appendChild(node);
+            }
 }
 class CheckboxField implements Field{
-
+    name:string;
+    label: string;
+    fieldType: FieldType;
+    value: any;
+    
+    constructor(value: any) {
+        this.name = "CheckboxField";
+        this.label = "etykieta"
+        this.fieldType = FieldType.CheckboxField;
+        this.value = value
+    }
+    render() {
+        var node=document.createElement("LI");
+            var textnode=document.createTextNode(this.name);
+            var inputnode=document.createElement("input");
+            node.appendChild(textnode);
+            node.appendChild(inputnode);
+            document.getElementById("namelist").appendChild(node);
+            }
 }
-*/
+
  class Form {
     fieldArray: Array<Field> = [];
-
     getValue() {
 
     }
 
     render() {
-        alert("AAA");
-        /*
+        
         let inputField: InputField = new InputField("A");
         inputField.render();
         this.fieldArray.push(inputField);
-        */
-        
 
+        let textAreaField: TextAreaField = new TextAreaField("A");
+        textAreaField.render();
+        this.fieldArray.push(textAreaField);
+
+        let dateField: DateField = new DateField("A");
+        dateField.render();
+        this.fieldArray.push(dateField);
+
+        let emailField: EmailField = new EmailField("A");
+        emailField.render();
+        this.fieldArray.push(emailField);
+
+        let selectField: SelectField = new SelectField("A");
+        selectField.render();
+        this.fieldArray.push(selectField);
+
+        let checkboxField: CheckboxField = new CheckboxField("A");
+        inputField.render();
+        this.fieldArray.push(checkboxField);
     }
 }
 
@@ -81,6 +190,6 @@ class App {
 
 }
 
-let form = new Form();
+ let format = new Form();
 
 
